235ae4ec8fafe67e13d2ed7c80b360d9dbba1fae
Chareeef <youssef.charif.h@gmail.com>
2023-10-09 23:06:27 +0100

    Correct UUID format

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,7 @@ class BaseModel():
     '''
 
     def __init__(self):
-        self.id = uuid.uuid4()
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
 
