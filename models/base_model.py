e4c3b10d78ae91a29c06c4f94e39601aff7036f3
Chareeef <youssef.charif.h@gmail.com>
2023-10-11 17:43:45 +0100

    Implement more classes

--- models/base_model.py
+++ models/base_model.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-'''This module implements the BaseModel class'''
+"""This module implements the BaseModel class"""
 import models
 import datetime
 import uuid
 
 
-class BaseModel():
-    '''
+class BaseModel:
+    """
     The BaseModel class
 
     Public instance attributes:
@@ -17,7 +17,7 @@ class BaseModel():
     Public instance methods:
         save(self)
         to_dict(self)
-    '''
+    """
 
     def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel object.
@@ -32,7 +32,7 @@ class BaseModel():
             for key, val in kwargs.items():
                 if key in ["created_at", "updated_at"]:
                     self.__dict__[key] = datetime.datetime.strptime(val, frmt)
-                elif key != '__class__':
+                elif key != "__class__":
                     self.__dict__[key] = val
         else:
             self.id = str(uuid.uuid4())
@@ -41,26 +41,26 @@ class BaseModel():
             models.storage.new(self)
 
     def save(self):
-        '''Updates `updated_at` with the current datetime'''
+        """Updates `updated_at` with the current datetime"""
 
         self.updated_at = datetime.datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        '''
+        """
         Returns a dictionary containing:
             - All keys/values of __dict__ of the instance
             - The key __class__ with the class name of the object
-        '''
+        """
 
         obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
+        obj_dict["__class__"] = self.__class__.__name__
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
 
         return obj_dict
 
     def __str__(self):
-        '''Return a human readable representation of the instance'''
+        """Return a human readable representation of the instance"""
 
-        return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
