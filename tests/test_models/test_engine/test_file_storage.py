bd8bcb308868c3766fe0f16996264f698e1b34e0
Chareeef <youssef.charif.h@gmail.com>
2023-10-12 09:54:50 +0100

    Test FileStorage with other classes

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -9,8 +9,14 @@ import models
 import unittest
 import os
 import shutil
-from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 
 class TestClass_instantiation(unittest.TestCase):
@@ -37,7 +43,7 @@ class TestMethods(unittest.TestCase):
     """testing methods of the FileStorage class."""
 
     @classmethod
-    def setUp(self):
+    def setUpClass(cls):
         try:
             shutil.copy2("file.json", "temp")
             FileStorage._FileStorage__file_path = "temp"
@@ -45,7 +51,7 @@ class TestMethods(unittest.TestCase):
             pass
 
     @classmethod
-    def tearDown(self):
+    def tearDownClass(cls):
         try:
             os.remove("temp")
             FileStorage._FileStorage__file_path = "file.json"
@@ -93,6 +99,299 @@ class TestMethods(unittest.TestCase):
     def test_reload_method_args(self):
         with self.assertRaises(TypeError):
             models.storage.reload(None)
+
+
+class TestWithUser(unittest.TestCase):
+    """testing that FileStorage class correctly handles User class"""
+
+    @classmethod
+    def setUpClass(cls):
+        try:
+            shutil.copy2("file.json", "temp")
+            FileStorage._FileStorage__file_path = "temp"
+        except IOError:
+            pass
+
+    @classmethod
+    def tearDownClass(cls):
+        try:
+            os.remove("temp")
+            FileStorage._FileStorage__file_path = "file.json"
+        except IOError:
+            pass
+        FileStorage._FileStorage__objects = {}
+
+    def test_new_method(self):
+        obj = User()
+        models.storage.new(obj)
+        self.assertIn("User." + obj.id, models.storage.all().keys())
+        self.assertIn(obj, models.storage.all().values())
+
+    def test_save_method(self):
+        obj = User()
+        models.storage.new(obj)
+        models.storage.save()
+        with open("temp", "r") as f:
+            storage_content = f.read()
+            self.assertIn("User." + obj.id, storage_content)
+
+    def test_reload_method(self):
+        obj = User()
+        key = "User." + obj.id
+        obj.name = 'Link'
+        models.storage.new(obj)
+        models.storage.save()
+        models.storage.reload()
+        storage_objs = FileStorage._FileStorage__objects
+        self.assertIn(key, storage_objs)
+        self.assertEqual(storage_objs[key].id, obj.id)
+        self.assertEqual(storage_objs[key].name, 'Link')
+
+
+class TestWithState(unittest.TestCase):
+    """testing that FileStorage class correctly handles State class"""
+
+    @classmethod
+    def setUpClass(cls):
+        try:
+            shutil.copy2("file.json", "temp")
+            FileStorage._FileStorage__file_path = "temp"
+        except IOError:
+            pass
+
+    @classmethod
+    def tearDownClass(cls):
+        try:
+            os.remove("temp")
+            FileStorage._FileStorage__file_path = "file.json"
+        except IOError:
+            pass
+        FileStorage._FileStorage__objects = {}
+
+    def test_new_method(self):
+        obj = State()
+        models.storage.new(obj)
+        self.assertIn("State." + obj.id, models.storage.all().keys())
+        self.assertIn(obj, models.storage.all().values())
+
+    def test_save_method(self):
+        obj = State()
+        models.storage.new(obj)
+        models.storage.save()
+        with open("temp", "r") as f:
+            storage_content = f.read()
+            self.assertIn("State." + obj.id, storage_content)
+
+    def test_reload_method(self):
+        obj = State()
+        key = "State." + obj.id
+        obj.name = 'California'
+        models.storage.new(obj)
+        models.storage.save()
+        models.storage.reload()
+        storage_objs = FileStorage._FileStorage__objects
+        self.assertIn(key, storage_objs)
+        self.assertEqual(storage_objs[key].id, obj.id)
+        self.assertEqual(storage_objs[key].name, 'California')
+
+
+class TestWithCity(unittest.TestCase):
+    """testing that FileStorage class correctly handles City class"""
+
+    @classmethod
+    def setUpClass(cls):
+        try:
+            shutil.copy2("file.json", "temp")
+            FileStorage._FileStorage__file_path = "temp"
+        except IOError:
+            pass
+
+    @classmethod
+    def tearDownClass(cls):
+        try:
+            os.remove("temp")
+            FileStorage._FileStorage__file_path = "file.json"
+        except IOError:
+            pass
+        FileStorage._FileStorage__objects = {}
+
+    def test_new_method(self):
+        obj = City()
+        models.storage.new(obj)
+        self.assertIn("City." + obj.id, models.storage.all().keys())
+        self.assertIn(obj, models.storage.all().values())
+
+    def test_save_method(self):
+        obj = City()
+        models.storage.new(obj)
+        models.storage.save()
+        with open("temp", "r") as f:
+            storage_content = f.read()
+            self.assertIn("City." + obj.id, storage_content)
+
+    def test_reload_method(self):
+        obj = City()
+        key = "City." + obj.id
+        obj.name = 'Tokyo'
+        models.storage.new(obj)
+        models.storage.save()
+        models.storage.reload()
+        storage_objs = FileStorage._FileStorage__objects
+        self.assertIn(key, storage_objs)
+        self.assertEqual(storage_objs[key].id, obj.id)
+        self.assertEqual(storage_objs[key].name, 'Tokyo')
+
+
+class TestWithPlace(unittest.TestCase):
+    """testing that FileStorage class correctly handles Place class"""
+
+    @classmethod
+    def setUpClass(cls):
+        try:
+            shutil.copy2("file.json", "temp")
+            FileStorage._FileStorage__file_path = "temp"
+        except IOError:
+            pass
+
+    @classmethod
+    def tearDownClass(cls):
+        try:
+            os.remove("temp")
+            FileStorage._FileStorage__file_path = "file.json"
+        except IOError:
+            pass
+        FileStorage._FileStorage__objects = {}
+
+    def test_new_method(self):
+        obj = Place()
+        models.storage.new(obj)
+        self.assertIn("Place." + obj.id, models.storage.all().keys())
+        self.assertIn(obj, models.storage.all().values())
+
+    def test_save_method(self):
+        obj = Place()
+        models.storage.new(obj)
+        models.storage.save()
+        with open("temp", "r") as f:
+            storage_content = f.read()
+            self.assertIn("Place." + obj.id, storage_content)
+
+    def test_reload_method(self):
+        obj = Place()
+        key = "Place." + obj.id
+        obj.name = 'Square Park'
+        obj.max_guest = 5
+        obj.latitude = 77.8
+        obj.longitude = 45.23
+        models.storage.new(obj)
+        models.storage.save()
+        models.storage.reload()
+        storage_objs = FileStorage._FileStorage__objects
+        self.assertIn(key, storage_objs)
+        self.assertEqual(storage_objs[key].id, obj.id)
+        self.assertEqual(type(storage_objs[key].id), str)
+        self.assertEqual(storage_objs[key].name, 'Square Park')
+        self.assertEqual(type(storage_objs[key].name), str)
+        self.assertEqual(storage_objs[key].max_guest, 5)
+        self.assertEqual(type(storage_objs[key].max_guest), int)
+        self.assertEqual(storage_objs[key].latitude, 77.8)
+        self.assertEqual(type(storage_objs[key].latitude), float)
+        self.assertEqual(storage_objs[key].longitude, 45.23)
+        self.assertEqual(type(storage_objs[key].longitude), float)
+
+
+class TestWithAmenity(unittest.TestCase):
+    """testing that FileStorage class correctly handles Amenity class"""
+
+    @classmethod
+    def setUpClass(cls):
+        try:
+            shutil.copy2("file.json", "temp")
+            FileStorage._FileStorage__file_path = "temp"
+        except IOError:
+            pass
+
+    @classmethod
+    def tearDownClass(cls):
+        try:
+            os.remove("temp")
+            FileStorage._FileStorage__file_path = "file.json"
+        except IOError:
+            pass
+        FileStorage._FileStorage__objects = {}
+
+    def test_new_method(self):
+        obj = Amenity()
+        models.storage.new(obj)
+        self.assertIn("Amenity." + obj.id, models.storage.all().keys())
+        self.assertIn(obj, models.storage.all().values())
+
+    def test_save_method(self):
+        obj = Amenity()
+        models.storage.new(obj)
+        models.storage.save()
+        with open("temp", "r") as f:
+            storage_content = f.read()
+            self.assertIn("Amenity." + obj.id, storage_content)
+
+    def test_reload_method(self):
+        obj = Amenity()
+        key = "Amenity." + obj.id
+        obj.name = 'Kitchen'
+        models.storage.new(obj)
+        models.storage.save()
+        models.storage.reload()
+        storage_objs = FileStorage._FileStorage__objects
+        self.assertIn(key, storage_objs)
+        self.assertEqual(storage_objs[key].id, obj.id)
+        self.assertEqual(storage_objs[key].name, 'Kitchen')
+
+
+class TestWithReview(unittest.TestCase):
+    """testing that FileStorage class correctly handles Review class"""
+
+    @classmethod
+    def setUpClass(cls):
+        try:
+            shutil.copy2("file.json", "temp")
+            FileStorage._FileStorage__file_path = "temp"
+        except IOError:
+            pass
+
+    @classmethod
+    def tearDownClass(cls):
+        try:
+            os.remove("temp")
+            FileStorage._FileStorage__file_path = "file.json"
+        except IOError:
+            pass
+        FileStorage._FileStorage__objects = {}
+
+    def test_new_method(self):
+        obj = Review()
+        models.storage.new(obj)
+        self.assertIn("Review." + obj.id, models.storage.all().keys())
+        self.assertIn(obj, models.storage.all().values())
+
+    def test_save_method(self):
+        obj = Review()
+        models.storage.new(obj)
+        models.storage.save()
+        with open("temp", "r") as f:
+            storage_content = f.read()
+            self.assertIn("Review." + obj.id, storage_content)
+
+    def test_reload_method(self):
+        obj = Review()
+        key = "Review." + obj.id
+        obj.text = 'Excellent'
+        models.storage.new(obj)
+        models.storage.save()
+        models.storage.reload()
+        storage_objs = FileStorage._FileStorage__objects
+        self.assertIn(key, storage_objs)
+        self.assertEqual(storage_objs[key].id, obj.id)
+        self.assertEqual(storage_objs[key].text, 'Excellent')
 
 
 if __name__ == "__main__":
